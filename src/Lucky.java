50cc6220ad23b700eb3c08211ba0422deb5c898f
mibelmay <zemcovannastasia11@gmail.com>
2023-11-06 02:16:26 +0500

    task 1

--- src/Lucky.java
+++ src/Lucky.java
@@ -1,16 +1,18 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Lucky {
-    static int x = 0;
-    static int count = 0;
+    static AtomicInteger count = new AtomicInteger(0);
+    static AtomicInteger x = new AtomicInteger(0);
 
     static class LuckyThread extends Thread {
         @Override
         public void run() {
-            while (x < 999999) {
-                x++;
-                if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
-                        % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
-                    System.out.println(x);
-                    count++;
+            int temp;
+            while ((temp = x.getAndIncrement()) < 999999) {
+                if ((temp % 10) + (temp / 10) % 10 + (temp / 100) % 10 == (temp / 1000)
+                        % 10 + (temp / 10000) % 10 + (temp / 100000) % 10) {
+                    System.out.println(temp);
+                    count.getAndIncrement();
                 }
             }
         }
